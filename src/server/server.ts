9e9e3921b8b4eca8a760e98c8dc3498ed1fac38b
Niakr1s <pavel2188@gmail.com>
2020-10-13 15:31:21 +0300

    added jest

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,10 @@
 import express from 'express'
-import path from 'path';
-import {fileURLToPath} from 'url'
+import path from 'path'
 
 const app = express()
 const port = +process.env.PORT || 3000
 
-const staticPath = path.join(path.dirname(fileURLToPath(import.meta.url)), '../../dist/www');
+const staticPath = path.join(__dirname, '../dist/www');
 app.use(express.static(staticPath))
 
 app.listen(port, () => {
